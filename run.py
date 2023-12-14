8a9366018a720f9db33e68fe8f9ddbf378237703
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-12-14 11:06:42 +0000

    Add plot generation and evaluation to the Keras model

--- run.py
+++ run.py
@@ -15,3 +15,74 @@ np.random.seed(seed_value)     # Semilla para NumPy
 tf.random.set_seed(seed_value) # Semilla para TensorFlow/Keras
 
 # A partir de aquí, puedes continuar con la construcción y entrenamiento de tu modelo de Keras
+def generatePlot(history):
+    # Extracción de datos
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+
+    # Creación del gráfico de accuracy
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, acc, 'b', label='Training')
+    plt.plot(epochs, val_acc, 'r', label='Validation')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # Creación del gráfico de loss
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, loss, 'b', label='Training')
+    plt.plot(epochs, val_loss, 'r', label='Validation')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+# MNIST
+# Cargar el conjunto de datos MNIST
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Preprocesar los datos (escalarlos entre 0 y 1 y convertir las etiquetas a one-hot encoding)
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32') / 255 # el 255 es para normalizar, 255 es el valor maximo de un pixel
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32') / 255
+# one-hot encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+#imprime una imagen
+plt.imshow(x_train[0].reshape(28, 28), cmap='gray')
+
+# Construir el modelo
+model = Sequential()
+# parte convolucional
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten()) # Aplanar la salida de la capa convolucional
+# parte fully connected (clasificador)
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5)) # Dropout para reducir overfitting
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+# Compilar el modelo
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrenar el modelo
+history_of_train = model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_data=(x_test, y_test))
+
+# Evaluar el modelo
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+generatePlot(history_of_train)
+
