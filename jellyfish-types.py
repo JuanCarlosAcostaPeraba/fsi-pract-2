2eddaf10b5a34dd0d84fad8587fd16bf933648c4
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2023-12-19 08:51:18 +0000

    Add Keras model training and plotting functions

--- jellyfish-types.py
+++ jellyfish-types.py
@@ -1,8 +1,55 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 import os
 
+# Establecer una semilla para reproducibilidad
+seed_value = 42
+random.seed(seed_value)        # Semilla para Python
+np.random.seed(seed_value)     # Semilla para NumPy
+tf.random.set_seed(seed_value) # Semilla para TensorFlow/Keras
+
+# A partir de aquí, puedes continuar con la construcción y entrenamiento de tu modelo de Keras
+
+def generatePlot(history):
+    # Extracción de datos
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+
+    # Creación del gráfico de accuracy
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, acc, 'b', label='Training')
+    plt.plot(epochs, val_acc, 'r', label='Validation')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # Creación del gráfico de loss
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, loss, 'b', label='Training')
+    plt.plot(epochs, val_loss, 'r', label='Validation')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 image_size = 150
-data_dir = os.path.join('data', 'jellyfish')
+data_dir = os.path.join('data2', 'jellyfish')
 batch_size = 16
 rescale_factor = 1./255
 
